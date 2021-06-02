35303f832d10efb6375a118205187215dd27d2d9
smita Raj <smita.raj12@gmail.com>
2021-06-02 11:09:09 -0700

    created a form that shows full address

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const IdInput = $("input#id").val();
+    const fnameInput = $("input#fname").val();
+    const mnameInput= $("input#mname").val();
+    const lnameInput = $("input#lname").val();
+    const hnoInput = $("input#hno").val();
+    const snameInput = $("input#sname").val();
+    const cityInput = $("input#city").val();
+    const codeInput = $("input#code").val();
+
+    $(".id").text(IdInput);
+    $(".fname").text(fnameInput);
+    $(".mname").text(mnameInput);
+    $(".lname").text(lnameInput);
+    $(".hno").text(hnoInput);
+    $(".sname").text(snameInput);
+    $(".city").text(cityInput);
+    $(".code").text(codeInput);
+
+    $("#address").show();
+  });
+});
